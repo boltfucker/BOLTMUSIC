cf3812a575ee03fbef0aa3b77f948133fe517753
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 19:31:35 +0530

    Update speed.py

--- PURVIMUSIC/plugins/admins/speed.py
+++ PURVIMUSIC/plugins/admins/speed.py
@@ -1,13 +1,13 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import SUDOERS, db
-from DAXXMUSIC.utils import AdminRightsCheck
-from DAXXMUSIC.utils.database import is_active_chat, is_nonadmin_chat
-from DAXXMUSIC.utils.decorators.language import languageCB
-from DAXXMUSIC.utils.inline import close_markup, speed_markup
+from PURVIMUSIC import app
+from PURVIMUSIC.core.call import PURVI
+from PURVIMUSIC.misc import SUDOERS, db
+from PURVIMUSIC.utils import AdminRightsCheck
+from PURVIMUSIC.utils.database import is_active_chat, is_nonadmin_chat
+from PURVIMUSIC.utils.decorators.language import languageCB
+from PURVIMUSIC.utils.inline import close_markup, speed_markup
 from config import BANNED_USERS, adminlist
 
 checker = []
@@ -94,7 +94,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await DAXX.speedup_stream(
+        await PURVI.speedup_stream(
             chat_id,
             file_path,
             speed,
