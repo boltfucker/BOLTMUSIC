4c98d9c804c8e6f6395a6e253bb781b1b74fb569
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 21:17:07 +0530

    Update imposter.py

--- PURVIMUSIC/plugins/tools/imposter.py
+++ PURVIMUSIC/plugins/tools/imposter.py
@@ -1,8 +1,8 @@
 import random 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from DAXXMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
-from DAXXMUSIC import app
+from PURVIMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
+from PURVIMUSIC import app
 
 MISHI = [
     "https://graph.org/file/f86b71018196c5cfe7344.jpg",
@@ -30,8 +30,8 @@ ROY = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
-            url=f"https://t.me/NexikoBot?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/DAXXSUPPORT")
+            url=f"https://t.me/Purvi_music_Bot?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/PURVI_UPDATES")
     ],
 ]
 
