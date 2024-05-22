447f6b028abd4a576666d202966c8efe26744b66
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-23 00:42:21 +0530

    Update logger.py

--- PURVIMUSIC/utils/logger.py
+++ PURVIMUSIC/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import is_on_off
+from PURVIMUSIC import app
+from PURVIMUSIC.utils.database import is_on_off
 from config import LOGGER_ID
 
 
