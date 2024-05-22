df6022ebd8621da3f58247b4ba37b7f732933340
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 19:05:55 +0530

    Update auth.py

--- PURVIMUSIC/plugins/admins/auth.py
+++ PURVIMUSIC/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils import extract_user, int_to_alpha
-from DAXXMUSIC.utils.database import (
+from PURVIMUSIC import app
+from PURVIMUSIC.utils import extract_user, int_to_alpha
+from PURVIMUSIC.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from DAXXMUSIC.utils.decorators import AdminActual, language
-from DAXXMUSIC.utils.inline import close_markup
+from PURVIMUSIC.utils.decorators import AdminActual, language
+from PURVIMUSIC.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
