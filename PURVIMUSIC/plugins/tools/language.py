863ce70cab243c78658204161700ebe203d43975
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 21:18:47 +0530

    Update language.py

--- PURVIMUSIC/plugins/tools/language.py
+++ PURVIMUSIC/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_lang, set_lang
-from DAXXMUSIC.utils.decorators import ActualAdminCB, language, languageCB
+from PURVIMUSIC import app
+from PURVIMUSIC.utils.database import get_lang, set_lang
+from PURVIMUSIC.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
