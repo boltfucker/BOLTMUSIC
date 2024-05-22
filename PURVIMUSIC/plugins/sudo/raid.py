a644bf783d03acb3baa89bd56ad7cf9e254f2d4c
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:32:17 +0530

    Update raid.py

--- PURVIMUSIC/plugins/sudo/raid.py
+++ PURVIMUSIC/plugins/sudo/raid.py
@@ -2,8 +2,8 @@ import pyrogram
 import time
 from pyrogram import filters
 from pyrogram import Client
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
+from PURVIMUSIC import app
+from PURVIMUSIC.misc import SUDOERS
 
 # Define the spam command handler
 @app.on_message(filters.command("raid", prefixes=".") & SUDOERS)
