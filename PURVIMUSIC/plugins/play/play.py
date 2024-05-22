ed767cc8a36d09f753f50be21c8b3cebc6fb1b15
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:22:33 +0530

    Update play.py

--- PURVIMUSIC/plugins/play/play.py
+++ PURVIMUSIC/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from DAXXMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils import seconds_to_min, time_to_seconds
-from DAXXMUSIC.utils.channelplay import get_channeplayCB
-from DAXXMUSIC.utils.decorators.language import languageCB
-from DAXXMUSIC.utils.decorators.play import PlayWrapper
-from DAXXMUSIC.utils.formatters import formats
-from DAXXMUSIC.utils.inline import (
+from PURVIMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from PURVIMUSIC.core.call import PURVI
+from PURVIMUSIC.utils import seconds_to_min, time_to_seconds
+from PURVIMUSIC.utils.channelplay import get_channeplayCB
+from PURVIMUSIC.utils.decorators.language import languageCB
+from PURVIMUSIC.utils.decorators.play import PlayWrapper
+from PURVIMUSIC.utils.formatters import formats
+from PURVIMUSIC.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from DAXXMUSIC.utils.logger import play_logs
-from DAXXMUSIC.utils.stream.stream import stream
+from PURVIMUSIC.utils.logger import play_logs
+from PURVIMUSIC.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
@@ -290,7 +290,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await DAXX.stream_call(url)
+                await PURVI.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
@@ -503,7 +503,7 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("DAXXmousAdmin") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("PURVImousAdmin") & ~BANNED_USERS)
 async def DAXXmous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
