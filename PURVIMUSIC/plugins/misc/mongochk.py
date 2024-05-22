7387bf480a3d3c44031d2ecef07f2d4bd29ac709
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:09:34 +0530

    Update mongochk.py

--- PURVIMUSIC/plugins/misc/mongochk.py
+++ PURVIMUSIC/plugins/misc/mongochk.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from DAXXMUSIC import app as DAXX
+from PURVIMUSIC import app as PURVI
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
