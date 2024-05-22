7d6b7a86453a7094e428f7f202210c38381e1c74
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:14:10 +0530

    Update seeker.py

--- PURVIMUSIC/plugins/misc/seeker.py
+++ PURVIMUSIC/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.database import get_active_chats, is_music_playing
+from PURVIMUSIC.misc import db
+from PURVIMUSIC.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
