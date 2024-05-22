b86a0ddd57f58c24b22c7e46278dc487b2ac5552
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 19:23:49 +0530

    Update seek.py

--- PURVIMUSIC/plugins/admins/seek.py
+++ PURVIMUSIC/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils import AdminRightsCheck, seconds_to_min
-from DAXXMUSIC.utils.inline import close_markup
+from PURVIMUSIC import YouTube, app
+from PURVIMUSIC.core.call import PURVI
+from PURVIMUSIC.misc import db
+from PURVIMUSIC.utils import AdminRightsCheck, seconds_to_min
+from PURVIMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await DAXX.seek_stream(
+        await PURVI.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
