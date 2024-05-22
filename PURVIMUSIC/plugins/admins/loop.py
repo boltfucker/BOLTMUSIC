5a2720a5cbc1c9d48633c54f4f8be2dcbc6042a0
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 19:16:58 +0530

    Update loop.py

--- PURVIMUSIC/plugins/admins/loop.py
+++ PURVIMUSIC/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_loop, set_loop
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from PURVIMUSIC import app
+from PURVIMUSIC.utils.database import get_loop, set_loop
+from PURVIMUSIC.utils.decorators import AdminRightsCheck
+from PURVIMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
