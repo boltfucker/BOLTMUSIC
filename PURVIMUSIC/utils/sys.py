6997a8a99e4878026aeb8ca1ba0606169f1fb84a
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-23 00:50:13 +0530

    Update sys.py

--- PURVIMUSIC/utils/sys.py
+++ PURVIMUSIC/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from DAXXMUSIC.misc import _boot_
-from DAXXMUSIC.utils.formatters import get_readable_time
+from PURVIMUSIC.misc import _boot_
+from PURVIMUSIC.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
