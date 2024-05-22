ee76dc77c464687679c41e6b8c6025807d760c08
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:29:05 +0530

    Update logger.py

--- PURVIMUSIC/plugins/sudo/logger.py
+++ PURVIMUSIC/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import add_off, add_on
-from DAXXMUSIC.utils.decorators.language import language
+from PURVIMUSIC import app
+from PURVIMUSIC.misc import SUDOERS
+from PURVIMUSIC.utils.database import add_off, add_on
+from PURVIMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
