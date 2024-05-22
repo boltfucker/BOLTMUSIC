046596501ae0a4801b5ab15324702414d4652a0c
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+purvimusic@users.noreply.github.com>
2024-05-22 20:29:48 +0530

    Update maintenance.py

--- PURVIMUSIC/plugins/sudo/maintenance.py
+++ PURVIMUSIC/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from PURVIMUSIC import app
+from PURVIMUSIC.misc import SUDOERS
+from PURVIMUSIC.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
